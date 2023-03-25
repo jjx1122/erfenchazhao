e1c371e4449ebb10d2980badc74a94a2a909c926
jjx1122 <328216858@qq.com>
2023-03-25 20:21:48 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class solution {
+public: 
+	int search(vector<int>& nums, int target) {
+		int left = 0;
+		int right = nums.size() - 1;
+		while (left <= right) {
+			int middle = (left + right) / 2;
+			if (nums[middle] > target)
+				right = middle - 1;
+			else if (nums[middle] < target)
+				left = middle + 1;
+			else {
+				cout << middle << endl;
+				return middle;
+			}
+				
+		}
+		return -1;//Î´ÕÒµ½
+	}
+};
+
+int main() {
+	vector<int>arr{ 1,2,3,5,8,10,13,17,20,21 };
+	solution a;
+	a.search(arr,5);
+	return 0;
+}
